ba57b97c9a6c41b72e642e8465c60a13dfef97d9
satyaM singh <satyamsingh92637@gmail.com>
2025-02-04 18:43:48 +0530

    Time: 1 ms (94.12%) | Memory: 42.5 MB (82.35%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,26 +1,21 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        if (nums.length == 0)
-            return 0;
-
         int maxLength = 1;
-        int inc = 1, dec = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                inc += 1;
-                dec = 1;
-            } else if (nums[i] < nums[i - 1]) {
-                dec += 1;
-                inc = 1;
+        int incLength = 1;
+        int decLength = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > nums[i - 1]) {
+                incLength++;
+                decLength = 1;
+            } else if(nums[i] < nums[i - 1]) {
+                decLength++;
+                incLength = 1;
             } else {
-                inc = 1;
-                dec = 1;
+                incLength = 1;
+                decLength = 1;
             }
-
-            maxLength = Math.max(maxLength, Math.max(inc, dec));
+            maxLength = Math.max(maxLength, Math.max(incLength, decLength));
         }
-
         return maxLength;
     }
 }
