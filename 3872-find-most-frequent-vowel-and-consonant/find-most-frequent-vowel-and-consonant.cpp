986662d4a69fef411b6e76fe2597c4d57a14f1c9
satyaM singh <satyamsingh92637@gmail.com>
2025-09-13 20:39:19 +0530

    Time: 5 ms (8.80%) | Memory: 9.2 MB (90.16%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxFreqSum(string& s) {
+        int freq[26]={0};
+        for(char c: s)
+            freq[c-'a']++;
+        constexpr unsigned mask=1+(1<<('e'-'a'))+(1<<('i'-'a'))+(1<<('o'-'a'))+(1<<('u'-'a'));
+    //    cout<<hex<<mask<<endl;
+        int maxCV[2]={0};// [0] for consonant [1] for vowel
+        for(int i=0; i<26; i++){
+            const bool idx=((1<<i) & mask)!=0;
+            maxCV[idx]=max(maxCV[idx], freq[i]);
+        }
+        return maxCV[0]+maxCV[1];
+    }
+};
