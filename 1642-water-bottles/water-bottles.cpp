44261de1775f2fa4ae587442fb059c384a0107e8
satyaM singh <satyamsingh92637@gmail.com>
2025-10-18 12:01:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) {
-        return numBottles + (numBottles - 1) / (numExchange - 1);
+    static int exchange(int n, int k){
+        if (n<k) return 0;
+        auto [n0, r]=div(n, k);
+        return n0+exchange(n0+r, k);
+    }
+    static int numWaterBottles(int n, int k) {
+        return n+exchange(n, k);
     }
 };
